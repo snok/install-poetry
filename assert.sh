5c425a2f97804a2bdcb6a53d613a3429770c27f3
Sondre Lillebo gundersen <sondrelg@otovo.com>
2020-10-26 14:38:06 +0100

    Trim assert.sh

--- assert.sh
+++ assert.sh
@@ -10,22 +10,3 @@ assert_in() {
     return 1
   fi
 }
-
-function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-
-assert_higher_than_1.1() {
-  if [ "$(version $1)" -ge "$(version "1.1.0")" ]; then
-      echo "Version ${1} is higher than 1.1.0"
-      return 1
-  fi
-  return 0
-}
-test() {
-  x=$1
-  y=$2
-  if [ "$(echo $x | sed  's/\.//g')" -lt "$(echo $y | sed  's/\.//g')" ]; then
-    echo "${x} is less than ${y}"
-  else
-    echo "${x} is not less than ${y}"
-  fi
-}
