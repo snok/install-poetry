9ed7c451911bd5b2dcbd85676390d014f8ebfa47
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 21:22:34 +0100

    Try again

--- main.sh
+++ main.sh
@@ -1,13 +1,6 @@
 if [ "$1" == "Windows" ]; then
-  echo "Windows block"
-  echo "%USERPROFILE%\.poetry\bin" >> $GITHUB_PATH
-  cd "%USERPROFILE%"
-  ls -la
-  echo "------------------"
-  cd "$2"
-  ls -la
+  pip install poetry
 else
-  echo "Else block"
   echo "$HOME/.poetry/bin" >> $GITHUB_PATH
   source $HOME/.poetry/env
 fi
