96dc5836b9051f6e63db87276a517497f779b206
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-05 00:04:15 +0100

    .

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 if [ "$1" == "Windows" ]; then
-  $HOME/.poetry/bin/poetry/poetry.bat
+  $HOME/.poetry/bin/poetry.bat
   ./poetry config virtualenvs.create "$2"
   ./poetry config virtualenvs.in-project "$3"
   ./poetry config virtualenvs.path "$4"
