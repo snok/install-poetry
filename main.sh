d873465aa363197a29e00e882b41dbc2d2e594eb
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 22:21:34 +0100

    .

--- main.sh
+++ main.sh
@@ -1,23 +1,6 @@
 if [ "$1" == "Windows" ]; then
 #  echo "%USERPROFILE%\.poetry\bin" >> $GITHUB_PATH
-  cd ..
-  cd ..
-  cd ..
-  cd ..
-  echo "-------- outer layer ----------"
-  ls -la
-  cd usr
-  cd bin
-  echo "------- usr/bin ----------"
-  ls -la
-  cd ..
-  cd ..
-  echo "-------- outer layer ----------"
-  ls -la
-  cd tmp
-  cd bin
-  echo "------- tmp/bin -----------"
-  ls -la
+  python -c 'import os; print(os.getenv("USERPROFILE", ""))'
 else
   echo "Else block"
   echo "$HOME/.poetry/bin" >> $GITHUB_PATH
