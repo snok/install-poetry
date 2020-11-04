847e3a44f41d891fff9f3104c273e1fc643e5c15
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 23:56:01 +0100

    .

--- main.sh
+++ main.sh
@@ -5,10 +5,10 @@ a
   echo "poetry=$HOME/.poetry/bin/poetry" >> $GITHUB_ENV
   printenv
   ls -la
-  ./poetry.bat
-  ./poetry config virtualenvs.create "$2"
-  ./poetry config virtualenvs.in-project "$3"
-  ./poetry config virtualenvs.path "$4"
+  poetry.bat
+  poetry config virtualenvs.create "$2"
+  poetry config virtualenvs.in-project "$3"
+  poetry config virtualenvs.path "$4"
 else
   source $HOME/.poetry/env
   poetry config virtualenvs.create "$2"
