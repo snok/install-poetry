f831fb7689c1534ba1a0bd6f315b88658ebfeb0b
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 22:45:34 +0100

    try again

--- main.sh
+++ main.sh
@@ -1,11 +1,12 @@
 if [ "$1" == "Windows" ]; then
   echo "$HOME/.poetry/bin" >> $GITHUB_PATH
   cd $HOME/.poetry/lib/poetry
-  ls -la
-  poetry
+  python poetry.py config virtualenvs.create "$2"
+  python poetry.py config virtualenvs.in-project "$3"
+  python poetry.py config virtualenvs.path "$4"
 else
   source $HOME/.poetry/env
+  poetry config virtualenvs.create "$2"
+  poetry config virtualenvs.in-project "$3"
+  poetry config virtualenvs.path "$4"
 fi
-poetry config virtualenvs.create "$2"
-poetry config virtualenvs.in-project "$3"
-poetry config virtualenvs.path "$4"
