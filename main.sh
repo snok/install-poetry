fcf149804f0cc023f7afbc4f1d7e33b72b166a00
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 21:04:28 +0100

    Try again

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 operating_system=$1
 echo "$operating_system"
-if [[ operating_system == 'Windows' ]]; then
+if [ "$1" == "Windows" ]; then
+  echo "Windows block"
   echo "%USERPROFILE%\.poetry\bin"
   cd "%USERPROFILE%\.poetry\lib"
 else
+  echo "Else block"
   echo "$HOME/.poetry/bin" >> $GITHUB_PATH
   source $HOME/.poetry/env
 fi
