a13ebfd7e1e8acc4aa31378fd48a04326260b183
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-05 00:02:07 +0100

    .

--- main.sh
+++ main.sh
@@ -1,14 +1,8 @@
 if [ "$1" == "Windows" ]; then
-a
-  printenv
-  echo "--------------"
-  echo "poetry=$HOME/.poetry/bin/poetry" >> $GITHUB_ENV
-  printenv
-  ls -la
-  poetry.bat
-  poetry config virtualenvs.create "$2"
-  poetry config virtualenvs.in-project "$3"
-  poetry config virtualenvs.path "$4"
+  $HOME/.poetry/bin/poetry/poetry.bat
+  ./poetry config virtualenvs.create "$2"
+  ./poetry config virtualenvs.in-project "$3"
+  ./poetry config virtualenvs.path "$4"
 else
   source $HOME/.poetry/env
   poetry config virtualenvs.create "$2"
