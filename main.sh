0b6615e1d1a9c30af8be3614b85099650e1eb09e
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 21:41:22 +0100

    Try to find the userprofile directory

--- main.sh
+++ main.sh
@@ -1,6 +1,12 @@
 if [ "$1" == "Windows" ]; then
-  pip install poetry
+#  echo "%USERPROFILE%\.poetry\bin" >> $GITHUB_PATH
+  cd ..
+  ls -la
+  echo "------------------"
+  cd ..
+  ls -la
 else
+  echo "Else block"
   echo "$HOME/.poetry/bin" >> $GITHUB_PATH
   source $HOME/.poetry/env
 fi
