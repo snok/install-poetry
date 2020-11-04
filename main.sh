075527b3efd17935bc558437ae274f49b80e9d30
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-04 22:49:04 +0100

    .

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 if [ "$1" == "Windows" ]; then
   echo "$HOME/.poetry/bin" >> $GITHUB_PATH
-  cd $HOME/.poetry/lib/poetry
-  python poetry.py config virtualenvs.create "$2"
-  python poetry.py config virtualenvs.in-project "$3"
-  python poetry.py config virtualenvs.path "$4"
+  cd $HOME/.poetry/bin
+  ls -la
+#  python poetry.py config virtualenvs.create "$2"
+#  python poetry.py config virtualenvs.in-project "$3"
+#  python poetry.py config virtualenvs.path "$4"
 else
   source $HOME/.poetry/env
   poetry config virtualenvs.create "$2"
