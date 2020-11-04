cd1ecf624ce93cb9c82a40d944cf5a2b2a9a322a
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-05 00:07:56 +0100

    2

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 if [ "$1" == "Windows" ]; then
   $HOME/.poetry/bin/poetry.bat
+  alias poetry="$HOME/.poetry/bin/poetry.bat"
   poetry config virtualenvs.create "$2"
   poetry config virtualenvs.in-project "$3"
   poetry config virtualenvs.path "$4"
