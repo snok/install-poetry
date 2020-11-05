588ba7bfeb7104299287cd3bc8f5124acfc5743a
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-11-05 21:10:27 +0100

    Try again

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 echo "$1"
+echo "--------------------------------------------------------------------------------------------------------"
 if [ $1 == Windows ]; then
-  echo -e '\nConfiguring Poetry for Windows!\n\nIf you are creating a venv in your project, you can activate it by running `source .venv/scripts/activate` 🚀'
+  echo -e '\n\nConfiguring Poetry for Windows!\n\nIf you are creating a venv in your project, you can activate it by running `source .venv/scripts/activate` 🚀\n\n'
   ln -s $HOME/.poetry/bin/poetry.bat poetry
   $HOME/.poetry/bin/poetry.bat config virtualenvs.create $2
   $HOME/.poetry/bin/poetry.bat config virtualenvs.in-project $3
   $HOME/.poetry/bin/poetry.bat config virtualenvs.path $4
 else
-  echo -e '\nConfiguring Poetry!\n\nIf you are creating a venv in your project, you can activate it by running `source .venv/bin/activate` 🚀'
+  echo -e '\n\nConfiguring Poetry!\n\nIf you are creating a venv in your project, you can activate it by running `source .venv/bin/activate` 🚀\n\n'
   source $HOME/.poetry/env
   poetry config virtualenvs.create $2
   poetry config virtualenvs.in-project $3
