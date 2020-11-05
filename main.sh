3ec343c07d5c486933e231a64f3a7eb7f525b789
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-11-05 21:31:02 +0100

    Try again

--- main.sh
+++ main.sh
@@ -1,15 +1,26 @@
-echo "$1"
-echo -e "\n\n  ----------------------------------------------------------------------------------------------------"
-if [ $1 == Windows ]; then
-  echo -e '\n\nConfiguring Poetry for Windows!\n\nIf you are creating a venv in your project, you can activate it by running `source .venv/scripts/activate` 🚀\n\n'
-  ln -s $HOME/.poetry/bin/poetry.bat poetry
-  $HOME/.poetry/bin/poetry.bat config virtualenvs.create $2
-  $HOME/.poetry/bin/poetry.bat config virtualenvs.in-project $3
-  $HOME/.poetry/bin/poetry.bat config virtualenvs.path $4
+# Help texts
+if [ "$1" == "Windows" ]; then
+  conf='Configuring Poetry for Windows!'
+  act='source .venv/scripts/activate'
 else
-  echo -e '\n\nConfiguring Poetry!\n\nIf you are creating a venv in your project, you can activate it by running `source .venv/bin/activate` 🚀\n\n'
+  conf='Configuring Poetry!'
+  act='source .venv/bin/activate'
+fi
+echo -e "\n\n----------------------------------------------------------------------------------------------------\n\n$conf"
+if [ $2 == true ] || [ "$2" == "true" ]; then
+    echo -e "\n\nIf you are creating a venv in your project, you can activate it by running `$act` 🚀"
+fi
+echo -e '\n\n----------------------------------------------------------------------------------------------------\n'
+
+# Poetry configuration
+if [ "$1" == "Windows" ]; then
+  ln -s "$HOME/.poetry/bin/poetry.bat" "poetry"
+  "$HOME/.poetry/bin/poetry.bat" config virtualenvs.create "$2"
+  "$HOME/.poetry/bin/poetry.bat" config virtualenvs.in-project "$3"
+  "$HOME/.poetry/bin/poetry.bat" config virtualenvs.path "$4"
+else
   source $HOME/.poetry/env
-  poetry config virtualenvs.create $2
-  poetry config virtualenvs.in-project $3
-  poetry config virtualenvs.path $4
+  poetry config virtualenvs.create "$2"
+  poetry config virtualenvs.in-project "$3"
+  poetry config virtualenvs.path "$4"
 fi
