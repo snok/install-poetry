56931b13c1cc5d830fb04ee5fc9abd3c60746262
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-11-05 19:38:13 +0100

    Try again

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 if [ "$1" == "Windows" ]; then
-  ln -s $HOME/.poetry/bin/poetry.bat poetry
+  ln -s $HOME/.poetry/bin/poetry.bat /usr/bin/poetry
+  poetry config virtualenvs.create "$2"
+  poetry config virtualenvs.in-project "$3"
+  poetry config virtualenvs.path "$4"
 else
   source $HOME/.poetry/env
+  poetry config virtualenvs.create "$2"
+  poetry config virtualenvs.in-project "$3"
+  poetry config virtualenvs.path "$4"
 fi
