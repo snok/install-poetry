ab794d819560183abb87c4862e86ee2e380131e4
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-11-05 23:42:17 +0100

    Test output

--- main.sh
+++ main.sh
@@ -1,20 +1,20 @@
 # Define OS specific help text
 if [ "$1" == "Windows" ]; then
-  conf="Configuring Poetry for Windows!"
+  conf="\033[33mConfiguring Poetry for Windows!\033[0m"
   act="source .venv/scripts/activate"
-  echo "VENV=.venv/scripts/activate" >> $GITHUB_ENV
+  echo "VENV=.venv/scripts/activate" >>$GITHUB_ENV
 else
-  conf="Configuring Poetry!"
+  conf="\033[33mConfiguring Poetry!\033[0m"
   act="source .venv/bin/activate"
-  echo "VENV=.venv/bin/activate" >> $GITHUB_ENV
+  echo "VENV=.venv/bin/activate" >>$GITHUB_ENV
 fi
 
 # Echo help texts
-echo -e "\n\n-------------------------------------------------------------------------------\n\n\033[33$conf\033[0m 🎉"
+echo -e "\n\n-------------------------------------------------------------------------------\n\n$conf 🎉"
 if [ $2 == true ] || [ "$2" == "true" ]; then
   # If user is creating a venv in-project we tell them how to activate venv
-  echo -e "\n\nIf you are creating a venv in your project, you can activate it by running '${act}'\033[0m"
-  echo -e "\n\nIf you're running this in an OS matrix, an even better option is to use 'source \$VENV'\033[0m"
+  echo -e "\n\n\033[33mIf you are creating a venv in your project, you can activate it by running '$act'\033[0m"
+  echo -e "\n\033[33mIf you're running this in an OS matrix, an even better option is to use 'source \$VENV'\033[0m"
 fi
 if [ "$1" == "Windows" ]; then
   # If $SHELL isn't some variation of bash, output a yellow-texted warning
