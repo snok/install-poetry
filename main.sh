a76f4f48dadb560b2d2b55b836e2d5f51597775e
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-11-05 21:42:39 +0100

    Try again

--- main.sh
+++ main.sh
@@ -2,18 +2,20 @@
 if [ "$1" == "Windows" ]; then
   conf="Configuring Poetry for Windows!"
   act="source .venv/scripts/activate"
+  echo "VENV=.venv/scripts/activate" >> $GITHUB_ENV
 else
   conf="Configuring Poetry!"
   act="source .venv/bin/activate"
+  echo "VENV=.venv/bin/activate" >> $GITHUB_ENV
 fi
 
 # Echo help texts
-echo -e "\n\n----------------------------------------------------------------------------------------------------\n$conf"
+echo -e "\n\n           -------------------------------------------------------------------------------           \n\n$conf"
 if [ $2 == true ] || [ "$2" == "true" ]; then
-  # If user is creating a venv in-project we tell them how to activate venv
-  echo -e "\n\nIf you are creating a venv in your project, you can activate it by running ${act} 🚀"
+  # If user is creating a venv in-project we tell them how to activate venv🎉
+  echo -e "\n\nIf you are creating a venv in your project, you can activate it by running '${act}' 🚀\n\nIf you're running this in an OS matrix, you can also use 'source $VENV' 🎉"
 fi
-echo -e '\n----------------------------------------------------------------------------------------------------\n'
+echo -e '\n           -------------------------------------------------------------------------------           \n'
 
 # Configure Poetry
 if [ "$1" == "Windows" ]; then
