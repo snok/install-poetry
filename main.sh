8f8733fcaa4d72902391a939bcb27dac8d011a0f
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-11-05 19:49:45 +0100

    Try matrix + symlink

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
-if [ "$1" == "Windows" ]; then
+if [ $1 == Windows ]; then
   ln -s $HOME/.poetry/bin/poetry.bat poetry
-  $HOME/.poetry/bin/poetry.bat config virtualenvs.create "$2"
-  $HOME/.poetry/bin/poetry.bat config virtualenvs.in-project "$3"
-  $HOME/.poetry/bin/poetry.bat config virtualenvs.path "$4"
+  $HOME/.poetry/bin/poetry.bat config virtualenvs.create $2
+  $HOME/.poetry/bin/poetry.bat config virtualenvs.in-project $3
+  $HOME/.poetry/bin/poetry.bat config virtualenvs.path $4
 else
   source $HOME/.poetry/env
-  poetry config virtualenvs.create "$2"
-  poetry config virtualenvs.in-project "$3"
-  poetry config virtualenvs.path "$4"
+  poetry config virtualenvs.create $2
+  poetry config virtualenvs.in-project $3
+  poetry config virtualenvs.path $4
 fi
