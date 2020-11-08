f70a98b97f17159d76341af0f56ae0392a0d444e
Sondre Lillebø Gundersen <sondrelg@live.no>
2020-11-08 12:37:00 +0100

    Add windows compatibility (#10)

--- assert.sh
+++ assert.sh
@@ -1,26 +1,28 @@
+assert_in_one() {
+  local option1="$1"
+  local option2="$2"
+  local option3="$3"
+  local long="$4"
+
+  if [[ "$long" == *"$option1"* || "$long" == *"$option2"* || "$long" == *"$option3"* ]]; then
+    echo "assertion succeeded: one of the options were found in ${long}"
+    return 0
+  else
+    echo "assertion failed: none of the options were found in ${long}"
+    echo -e "options:\n- $option1\n- $option2\n- $option3"
+    return 1
+  fi
+}
 assert_in() {
   local short="$1"
   local long="$2"
-
+  # On windows paths use \ and unix uses /, so here we standardize
+  long="${long//\\//}"
   if [[ "$long" == *"$short"* ]]; then
     echo "assertion succeeded: ${short} was found in ${long}"
     return 0
   else
     echo "assertion failed: ${short} not found in ${long}"
-    return 1
-  fi
-}
-
-assert_in_one_or_the_other() {
-  local option1="$1"
-  local option2="$2"
-  local long="$3"
-
-  if [[ "$long" == *"$option1"* || "$long" == *"$option2"* ]]; then
-    echo "assertion succeeded: one of the options were found in ${long}"
-    return 0
-  else
-    echo "assertion failed: neither option was found in ${long}"
     return 1
   fi
 }
