97c3b34b32341710cf3cfa0af03fa9b93a9dae4e
Sondre Lillebø Gundersen <sondrelg@live.no>
2021-07-28 08:20:19 +0200

    Reorder main.sh to install Poetry before outputting help text

--- scripts/v1.2/main.sh
+++ scripts/v1.2/main.sh
@@ -8,42 +8,22 @@ venv_path=$4
 version=$5
 installation_script=$6
 
-# Define OS specific help text
-if [ "$os" == "Windows" ]; then
-  conf="\033[33mConfiguring Poetry for Windows!\033[0m"
-  act="source .venv/scripts/activate"
-  echo "VENV=.venv/scripts/activate" >>"$GITHUB_ENV"
-else
-  conf="\033[33mConfiguring Poetry!\033[0m"
-  act="source .venv/bin/activate"
-  echo "VENV=.venv/bin/activate" >>"$GITHUB_ENV"
-fi
-
-# Echo help texts
-echo -e "\n\n-------------------------------------------------------------------------------\n\n$conf 🎉"
-if [ "$venv_create" == true ] || [ "$venv_create" == "true" ]; then
-  # If user is creating a venv in-project we tell them how to activate venv
-  echo -e "\n\n\033[33mIf you are creating a venv in your project, you can activate it by running '$act'\033[0m"
-  echo -e "\n\033[33mIf you're running this in an OS matrix, use 'source \$VENV'\033[0m"
-fi
-if [ "$os" == "Windows" ]; then
-  # If $SHELL isn't some variation of bash, output a yellow-texted warning
-  echo -e "\n\n\033[33mMake sure to set your default shell to bash when on Windows.\033[0m"
-  echo -e "\n\033[33mSee the github action docs for more information and examples.\033[0m"
-fi
-echo -e '\n-------------------------------------------------------------------------------\n'
-
+# Set path for each OS - done because Poetry inference is inconsistent
+# on mac-runners, and we need to know the install path so we can add it to $GITHUB_PATH
 if [ "$os" == "Windows" ]; then
   path="C:/Users/runneradmin/AppData/Roaming/Python/Scripts/"
 else
   path="$HOME/.local/"
 fi
 
+# Install Poetry
 POETRY_HOME=$path python3 $installation_script --yes --version=$version
 
+# Add to path
 echo "$path/bin" >>$GITHUB_PATH
 export PATH="$path/bin:$PATH"
 
+# Configure Poetry
 if [ "$os" == "Windows" ]; then
   # Adding to path on windows doesn't immediately take effect
   # so calling the executable directly here - should be available
@@ -57,3 +37,27 @@ else
   poetry config virtualenvs.path "$venv_path"
 fi
 
+# Define OS specific help texts
+if [ "$os" == "Windows" ]; then
+  conf="\033[33mConfiguring Poetry for Windows!\033[0m"
+  act="source .venv/scripts/activate"
+  echo "VENV=.venv/scripts/activate" >>"$GITHUB_ENV"
+else
+  conf="\033[33mConfiguring Poetry!\033[0m"
+  act="source .venv/bin/activate"
+  echo "VENV=.venv/bin/activate" >>"$GITHUB_ENV"
+fi
+
+# Output help texts
+echo -e "\n\n-------------------------------------------------------------------------------\n\n$conf 🎉"
+if [ "$venv_create" == true ] || [ "$venv_create" == "true" ]; then
+  # If user is creating a venv in-project we tell them how to activate venv
+  echo -e "\n\n\033[33mIf you are creating a venv in your project, you can activate it by running '$act'\033[0m"
+  echo -e "\n\033[33mIf you're running this in an OS matrix, use 'source \$VENV'\033[0m"
+fi
+if [ "$os" == "Windows" ]; then
+  # If $SHELL isn't some variation of bash, output a yellow-texted warning
+  echo -e "\n\n\033[33mMake sure to set your default shell to bash when on Windows.\033[0m"
+  echo -e "\n\033[33mSee the github action docs for more information and examples.\033[0m"
+fi
+echo -e '\n-------------------------------------------------------------------------------\n'
