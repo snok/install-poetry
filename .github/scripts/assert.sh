c5011cb6f071b3fb0274c5a858882c60ea1f20cc
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-10-07 08:15:35 +0800

    chore: Change shfmt indentation to 2 spaces

--- .github/scripts/assert.sh
+++ .github/scripts/assert.sh
@@ -3,30 +3,30 @@
 set -eo pipefail
 
 assert_in_one() {
-    local option1="$1"
-    local option2="$2"
-    local option3="$3"
-    local long="$4"
+  local option1="$1"
+  local option2="$2"
+  local option3="$3"
+  local long="$4"
 
-    if [[ "$long" == *"$option1"* || "$long" == *"$option2"* || "$long" == *"$option3"* ]]; then
-        echo "assertion succeeded: one of the options were found in ${long}"
-        return 0
-    else
-        echo "assertion failed: none of the options were found in ${long}"
-        echo -e "options:\n- $option1\n- $option2\n- $option3"
-        return 1
-    fi
+  if [[ "$long" == *"$option1"* || "$long" == *"$option2"* || "$long" == *"$option3"* ]]; then
+    echo "assertion succeeded: one of the options were found in ${long}"
+    return 0
+  else
+    echo "assertion failed: none of the options were found in ${long}"
+    echo -e "options:\n- $option1\n- $option2\n- $option3"
+    return 1
+  fi
 }
 assert_in() {
-    local short="$1"
-    local long="$2"
-    # On windows paths use \ and unix uses /, so here we standardize
-    long="${long//\\//}"
-    if [[ "$long" == *"$short"* ]]; then
-        echo "assertion succeeded: ${short} was found in ${long}"
-        return 0
-    else
-        echo "assertion failed: ${short} not found in ${long}"
-        return 1
-    fi
+  local short="$1"
+  local long="$2"
+  # On windows paths use \ and unix uses /, so here we standardize
+  long="${long//\\//}"
+  if [[ "$long" == *"$short"* ]]; then
+    echo "assertion succeeded: ${short} was found in ${long}"
+    return 0
+  else
+    echo "assertion failed: ${short} not found in ${long}"
+    return 1
+  fi
 }
--- scripts/v1.1/main.sh
+++ scripts/v1.1/main.sh
@@ -8,38 +8,38 @@ venv_path=$4
 
 # Define OS specific help text
 if [ "$os" == "Windows" ]; then
-    conf="\033[33mConfiguring Poetry for Windows!\033[0m"
-    act="source .venv/scripts/activate"
-    echo "VENV=.venv/scripts/activate" >>"$GITHUB_ENV"
+  conf="\033[33mConfiguring Poetry for Windows!\033[0m"
+  act="source .venv/scripts/activate"
+  echo "VENV=.venv/scripts/activate" >>"$GITHUB_ENV"
 else
-    conf="\033[33mConfiguring Poetry!\033[0m"
-    act="source .venv/bin/activate"
-    echo "VENV=.venv/bin/activate" >>"$GITHUB_ENV"
+  conf="\033[33mConfiguring Poetry!\033[0m"
+  act="source .venv/bin/activate"
+  echo "VENV=.venv/bin/activate" >>"$GITHUB_ENV"
 fi
 
 # Echo help texts
 echo -e "\n\n-------------------------------------------------------------------------------\n\n$conf 🎉"
 if [ "$venv_create" == true ] || [ "$venv_create" == "true" ]; then
-    # If user is creating a venv in-project we tell them how to activate venv
-    echo -e "\n\n\033[33mIf you are creating a venv in your project, you can activate it by running '$act'\033[0m"
-    echo -e "\n\033[33mIf you're running this in an OS matrix, use 'source \$VENV'\033[0m"
+  # If user is creating a venv in-project we tell them how to activate venv
+  echo -e "\n\n\033[33mIf you are creating a venv in your project, you can activate it by running '$act'\033[0m"
+  echo -e "\n\033[33mIf you're running this in an OS matrix, use 'source \$VENV'\033[0m"
 fi
 if [ "$os" == "Windows" ]; then
-    # If $SHELL isn't some variation of bash, output a yellow-texted warning
-    echo -e "\n\n\033[33mMake sure to set your default shell to bash when on Windows.\033[0m"
-    echo -e "\n\033[33mSee the package docs for more information and examples.\033[0m"
+  # If $SHELL isn't some variation of bash, output a yellow-texted warning
+  echo -e "\n\n\033[33mMake sure to set your default shell to bash when on Windows.\033[0m"
+  echo -e "\n\033[33mSee the package docs for more information and examples.\033[0m"
 fi
 echo -e '\n-------------------------------------------------------------------------------\n'
 
 # Configure Poetry
 if [ "$os" == "Windows" ]; then
-    ln -s "$HOME/.poetry/bin/poetry.bat" "poetry"
-    "$HOME/.poetry/bin/poetry.bat" config virtualenvs.create "$venv_create"
-    "$HOME/.poetry/bin/poetry.bat" config virtualenvs.in-project "$venv_in_project"
-    "$HOME/.poetry/bin/poetry.bat" config virtualenvs.path "$venv_path"
+  ln -s "$HOME/.poetry/bin/poetry.bat" "poetry"
+  "$HOME/.poetry/bin/poetry.bat" config virtualenvs.create "$venv_create"
+  "$HOME/.poetry/bin/poetry.bat" config virtualenvs.in-project "$venv_in_project"
+  "$HOME/.poetry/bin/poetry.bat" config virtualenvs.path "$venv_path"
 else
-    source "$HOME"/.poetry/env
-    poetry config virtualenvs.create "$venv_create"
-    poetry config virtualenvs.in-project "$venv_in_project"
-    poetry config virtualenvs.path "$venv_path"
+  source "$HOME"/.poetry/env
+  poetry config virtualenvs.create "$venv_create"
+  poetry config virtualenvs.in-project "$venv_in_project"
+  poetry config virtualenvs.path "$venv_path"
 fi
