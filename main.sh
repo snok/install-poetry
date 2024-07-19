14bca50f73e44bec0966a813897171b9ac58c10f
Sergey Ivanychev <sergeyivanychev@gmail.com>
2024-07-19 20:57:43 +0200

    Fix command

--- main.sh
+++ main.sh
@@ -11,9 +11,9 @@ INSTALLATION_SCRIPT="$(mktemp)"
 GET_SCRIPT_CMD="python3 -c 'import urllib.request, sys; print(urllib.request.urlopen(f\"{sys.argv[1]}\").read().decode(\"utf8\"))'"
 
 if [ "${RUNNER_OS}" == "Windows" ]; then
-  "${GET_SCRIPT_CMD}" https://raw.githubusercontent.com/python-poetry/poetry/48339106eb0d403a3c66519317488c8185844b32/install-poetry.py > "$INSTALLATION_SCRIPT"
+  ${GET_SCRIPT_CMD} https://raw.githubusercontent.com/python-poetry/poetry/48339106eb0d403a3c66519317488c8185844b32/install-poetry.py > "$INSTALLATION_SCRIPT"
 else
-  "${GET_SCRIPT_CMD}" https://install.python-poetry.org/ > "$INSTALLATION_SCRIPT"
+  ${GET_SCRIPT_CMD} https://install.python-poetry.org/ > "$INSTALLATION_SCRIPT"
 fi
 
 echo -e "\n${YELLOW}Setting Poetry installation path as $INSTALL_PATH${RESET}\n"
